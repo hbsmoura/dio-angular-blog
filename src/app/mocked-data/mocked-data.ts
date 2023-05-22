41a597f29befb3b35e1b46f473b098dafa0e4ae8
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-22 18:41:39 -0300

    create mocked data

---
+++ src/app/mocked-data/mocked-data.ts
@@ -0,0 +1,38 @@
+export const fakeData = [
+    {
+        id: 1,
+        pic: {
+            src: 'https://repository.compasquality.siemens-info.com/bitbucket/projects/SPRING_BOOT_TUTORIAL/avatar.png?s=256&v=1565755686335',
+            alt: 'Spring framework\' logo'
+        },
+        title: 'Creating REST APIs with Spring Boot',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum similique, neque facere veniam ullam, numquam eos cupiditate nulla nobis, atque placeat fuga expedita amet ipsam.'
+    },
+    {
+        id: 2,
+        pic: {
+            src: 'https://bognarjunior.files.wordpress.com/2014/12/1417589466_angular-symbol-256.png',
+            alt: 'Angular framework\'s logo'
+        },
+        title: 'Working with angular components',
+        desc: 'Pariatur voluptate omnis animi explicabo beatae natus, sint, quidem voluptatibus sequi sed delectus similique reiciendis quasi repellendus quia fugit inventore eligendi cumque.'
+    },
+    {
+        id: 3,
+        pic: {
+            src: 'https://st6.io/static/c7000851b7da77de65b2e19fda7f679c/bootstrap-and-tailwind.jpeg',
+            alt: 'Bootstrap CSS and Twailwind CSS logos'
+        },
+        title: 'Bootstrap vs Twailwind: a battle with style',
+        desc: 'Labore sequi maxime aliquam fuga ipsum consequuntur officiis iste facere voluptates nemo velit obcaecati repudiandae asperiores, dolorem consequatur odit quaerat veniam sed?'
+    },
+    {
+        id: 4,
+        pic: {
+            src: 'https://www.chemist-web.com/images/html_css_js_t.jpg',
+            alt: 'HTML, CSS and Javascript logos'
+        },
+        title: 'Frontend essential technologies',
+        desc: 'Consectetur officia aperiam vitae, corrupti similique est nostrum sed odio nisi enim, non quaerat ex delectus? Sit eligendi a quos incidunt animi!'
+    }
+]
