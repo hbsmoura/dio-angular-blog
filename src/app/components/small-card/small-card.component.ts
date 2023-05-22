09c053e50d92ebd3df7356c3ebab58be4ecdc081
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-22 11:38:37 -0300

    make small-card component dymanic

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+    @Input() pic: string = 'https://cdn.vectorstock.com/i/1000x1000/35/52/placeholder-rgb-color-icon-vector-32173552.webp'
+    @Input() picAlt: string = 'Card picture'
+    @Input() title: string = 'Title'
 }
